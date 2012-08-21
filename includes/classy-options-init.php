6debba58e2378079124a3efdd00fa17832066d83
hugosaner <hugo@sanerdesign.com>
2012-08-21 12:05:42 +0100

    add custom url to logo
    Signed-off-by: hugosaner <hugo@sanerdesign.com>

--- includes/classy-options-init.php
+++ includes/classy-options-init.php
@@ -110,6 +110,7 @@ $options
 			->subsection("Header Options")
 			->checkbox($themeslug."_custom_logo", "Custom Logo" , array('default' => true))
 			->upload($themeslug."_logo", "Logo", array('default' => array('url' => TEMPLATE_URL . '/images/responselogo.png')))
+			->text($themeslug."_logo_url", "Logo Custom URL", array('default' => home_url()))
 			->upload($themeslug."_favicon", "Custom Favicon")
 			->upload($themeslug."_apple_touch", "Apple Touch Icon", array('default' => array('url' => TEMPLATE_URL . '/images/apple-icon.png')))
 		->subsection_end()
